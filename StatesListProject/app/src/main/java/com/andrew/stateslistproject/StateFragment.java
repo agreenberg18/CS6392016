b7487efa9d2e630002b9b66ae2a75e4fb282d660
agreenberg18 <ag98718n@pace.edu>
2016-06-29 22:55:25 -0400

    Moved logic from mainActivity to StateFragment

--- StatesListProject/app/src/main/java/com/andrew/stateslistproject/StateFragment.java
+++ StatesListProject/app/src/main/java/com/andrew/stateslistproject/StateFragment.java
@@ -34,77 +34,77 @@ public class StateFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.state_fragment_layout, container, false);
-//        String [] states  = {"Alabama",
-//                "Alaska",
-//                "Arizona",
-//                "Arkansas",
-//                "California",
-//                "Colorado",
-//                "Connecticut",
-//                "Delaware",
-//                "Florida",
-//                "Georgia",
-//                "Hawaii",
-//                "Idaho",
-//                "Illinois",
-//                "Indiana",
-//                "Iowa",
-//                "Kansas",
-//                "Kentucky",
-//                "Louisiana",
-//                "Maine",
-//                "Maryland",
-//                "Massachusetts",
-//                "Michigan",
-//                "Minnesota",
-//                "Mississippi",
-//                "Missouri",
-//                "Montana",
-//                "Nebraska",
-//                "Nevada",
-//                "New Hampshire",
-//                "New Jersey",
-//                "New Mexico",
-//                "New York",
-//                "North Carolina",
-//                "North Dakota",
-//                "Ohio",
-//                "Oklahoma",
-//                "Oregon",
-//                "Pennsylvania",
-//                "Rhode Island",
-//                "South Carolina",
-//                "South Dakota",
-//                "Tennessee",
-//                "Texas",
-//                "Utah",
-//                "Vermont",
-//                "Virginia",
-//                "Washington",
-//                "West Virginia",
-//                "Wisconsin",
-//                "Wyoming"
-//        };
-//        List<String> stateList = new ArrayList<String>(Arrays.asList(states));
-//        ArrayAdapter<String> stateAdapter = new ArrayAdapter<String>(StateFragment.this, R.layout.activity_main, R.id.textView, stateList);
-//
-//        ListView lv = (ListView) v.findViewById(R.id.listView);
-//        lv.setAdapter(stateAdapter);
-//
-//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-//            public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
-//                if (pos == 38){
-//                    map();
-//                }
-//            }
-//        });
+        String [] states  = {"Alabama",
+                "Alaska",
+                "Arizona",
+                "Arkansas",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Iowa",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Maine",
+                "Maryland",
+                "Massachusetts",
+                "Michigan",
+                "Minnesota",
+                "Mississippi",
+                "Missouri",
+                "Montana",
+                "Nebraska",
+                "Nevada",
+                "New Hampshire",
+                "New Jersey",
+                "New Mexico",
+                "New York",
+                "North Carolina",
+                "North Dakota",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "Rhode Island",
+                "South Carolina",
+                "South Dakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Vermont",
+                "Virginia",
+                "Washington",
+                "West Virginia",
+                "Wisconsin",
+                "Wyoming"
+        };
+        List<String> stateList = new ArrayList<String>(Arrays.asList(states));
+        ArrayAdapter<String> stateAdapter = new ArrayAdapter<String>(getActivity(), R.layout.state_fragment_layout, R.id.textView, stateList);
+
+        ListView lv = (ListView) v.findViewById(R.id.listView);
+        lv.setAdapter(stateAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
+                if (pos == 38){
+                    map();
+                }
+            }
+        });
         return v;
     }
-//    protected void map() {
-//        final Intent intent = new Intent(Intent.ACTION_VIEW);
-//        intent.setData(Uri.parse("geo:0,0?q=Providence+Rhode+Island"));
-//        if (intent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(intent);
-//        }
-//    }
+    protected void map() {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("geo:0,0?q=Providence+Rhode+Island"));
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
